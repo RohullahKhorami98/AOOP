56e87cc9a970fb5e54b129287daa3c540c603fca
williamswe123 <44167244+williamswe123@users.noreply.github.com>
2020-04-29 14:34:32 +0200

    Update Pretty_Print_Trees.java

--- Assignment_two/Pretty_Print_Trees.java
+++ Assignment_two/Pretty_Print_Trees.java
@@ -3,19 +3,19 @@ package Visitorbase;
 public class Pretty_Print_Trees<T> implements TreeVisitor<T, String> {
 
 	boolean firstTime = true;
-	int count = 1;
-
+	int depth;
 	@Override
 	public String visit(Tree<T> t, String initialResult) {
 		// VISIT TREE - START
+		depth = 0;
 		return t.accept(this, initialResult);
 	}
 
 	@Override
 	public String visit(Leaf<T> l, String initialResult) {
 		// VISIT LEAF
-
-		initialResult = initialResult + "\n \t" + l.getValue();
+		initialResult = initialResult + "\n";
+		initialResult = initialResult + "\t" + l.getValue();
 		return initialResult;
 	}
 
@@ -24,13 +24,20 @@ public class Pretty_Print_Trees<T> implements TreeVisitor<T, String> {
 		// VISIT NODE
 		if (firstTime) {
 			firstTime = false;
+			depth--;
 		} else {
-			initialResult = initialResult + " \n NODE " + count++ + ":";
+			initialResult = initialResult + " \n";
+			System.out.println(depth);
+			for (int i = 0; i<depth;i++) {
+				initialResult = initialResult + "\t";
+			}
+			initialResult = initialResult + "NODE:";
 		}
+		depth++;
 		for (Tree<T> child : n.getChildren()) {
 			initialResult = child.accept(this, initialResult);
 		}
+		depth--;
 		return initialResult;
 	}
-
 }
