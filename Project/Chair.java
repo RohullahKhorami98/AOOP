a6b42521cd98653d46ed6c17af51760dc11228d8
Rohullah <rohullah.khorami98@gmail.com>
2020-05-05 14:34:58 +0200

    changed alot

--- Project/Chair.java
+++ Project/Chair.java
@@ -1,3 +1,6 @@
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -5,9 +8,21 @@ import javax.swing.JLabel;
 @SuppressWarnings("serial")
 public class Chair extends JLabel {
 	public BufferedImage image;
-
+	public Point Position;
+	public Point PositionOnScreen;
 	public Chair() {
 		new JLabel();
 		setIcon(new ImageIcon("/Users/rohullahkhorami/Desktop/AOOP/Assginement/Project/src/chair.png"));
+		addMouseMotionListener(new MouseAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				Position = getLocation();
+				PositionOnScreen = getLocationOnScreen();
+				Point temp = e.getLocationOnScreen();
+				int x = temp.x - PositionOnScreen.x + Position.x;
+				int y = temp.y - PositionOnScreen.y + Position.y;
+				setLocation(x, y);
+			}
+		});
 	}
 }
